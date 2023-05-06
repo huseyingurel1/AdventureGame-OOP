14384018b4dfc2e6626b066b67feafe1bd77750e
huseyingurel1 <huseyingurel27@ogr.atauni.edu.tr>
2023-05-06 20:35:11 +0300

    SafeHouse and ToolStore added

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,50 @@
 import java.util.Scanner;
 
 public class Game {
-
     private Scanner scanner = new Scanner(System.in);
 
     public void start() {
         System.out.println(" Welcome to Adventure Game! ");
         System.out.println(" Please enter your name  :  ");
 
-        // String playerName = scanner.nextLine();
+        String playerName = scanner.nextLine();
+        Player player = new Player(playerName);
 
-        Player player = new Player("Hüseyin");
+//        Player player = new Player("Hüseyin");
 
-        System.out.println(player.getName() + "   I want to play game with you ! Are you ready ? ");
+        System.out.println(player.getName() + ", I want to play game with you ! Are you ready ? ");
 
         System.out.println("Select a character for start the game");
 
         player.selectChar();
 
+        Location location = null;
 
+        while(true){
+            System.out.println();
+            System.out.println("############## Locations ##############");
+            System.out.println();
+            System.out.println("1- SafeHouse  --> It's safe for you here, there are no enemies here ");
+            System.out.println("2- ToolStore  --> You can buy weapons or armor here ");
+            System.out.println("Please select the location you want to go to :  ");
+            int selectLoc = scanner.nextInt();
 
+            switch (selectLoc){
+                case 1 :
+                    location = new SafeHouse(player);
+                    break;
+                case 2 :
+                    location = new ToolStore(player);
+                    break;
+                case 3 :
+                    location = new SafeHouse(player);
+            }
 
+            if(!location.onLocation())
+            {
+                System.out.println("Game Over!");
+                break;
+            }
+        }
     }
 }
---
+++ src/ToolStore.java
@@ -0,0 +1,34 @@
+public class ToolStore extends NormalLoc{
+    public ToolStore(Player player) {
+        super(player, "ToolStore");
+    }
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("------ Welcome to ToolStore ! ------");
+        System.out.println("1 - Weapons");
+        System.out.println("2 - Armors");
+        System.out.println("3 - Exit");
+        System.out.println("Your choice : ");
+        int selectCase = scanner.nextInt();
+        while( selectCase < 1 && selectCase > 3){
+            System.out.println("Invalid choice, please try again : ");
+            selectCase = scanner.nextInt();
+        }
+
+        switch(selectCase){
+            // TODO: 5/6/2023 print methodları yazılacak
+            case 1 :
+//                printWeapons();
+                break;
+            case 2 :
+//                printArmors();
+                break;
+            case 3 :
+                System.out.println("exit kodu yazılacak");
+                break;
+            default :
+        }
+        return true;
+    }
+}
